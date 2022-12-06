0f0a7b26888c9260e48e762251a73ea46fd19671
Tom de Geus <tom@geus.me>
2022-12-06 10:18:26 +0100

    Simplifying implementation

--- python/GooseEPM/__init__.py
+++ python/GooseEPM/__init__.py
@@ -1,37 +1,60 @@
 from ._GooseEPM import *  # noqa: F401, F403
+
 import scipy.fftpack as fft
 import numpy as np
 
-def generate_propagator(L, method = 'rossi'):
-    """Generates a periodic Eshelby-like propagator, according to the method described in Rossi et al. (2022). It is meant to be used for strain-controlled protocol.
+def elshelby_propagator(L: int, imposed = "strain") -> np.ndarray:
+    """
+    Generates a periodic Eshelby-like propagator.
+    The convention that is followed is:
 
-    Args:
-        L (int): length of the system
-        method (str, optional): As legacy, 'marko' can be used to generate the propagator like in the original EPM code provided by Marko Popovic. Defaults to 'rossi'.
+    -   ``dx = dx = [0, 1, 2, ..., L / 2, -L / 2 + 1, ..., -1]``
+    -   ``propagator[0, 0] = -1``
+    -   Imposed strain: ``np.mean(propagator) = -1 / propagator.size``
+    -   Imposed stress: ``np.mean(propagator) = 0``
 
-    Returns:
-        ndarray: The propagator in real space, centered in [0,0].
+    See also: Rossi, Biroli, Ozawa, Tarjus, Zamponi (2022), https://arxiv.org/abs/2204.10683
+
+    :param L: Linear size (in pixels) of the square propagator.
+    :param imposed: ``"strain"`` or ``"stress"``.
+    :return: ``(propagator, dx, dy)``
     """
     qx = fft.fftfreq(L) * 2*np.pi
-    qy = qx.copy()
-    qx, qy = np.meshgrid(qx,qy)
+    qx, qy = np.meshgrid(qx,qx)
 
     a = 2-2*np.cos(qx)
     b = 2-2*np.cos(qy)
+    q = (a+b)**2
+    q[:, 0] = 1
+    q[0, :] = 1
 
-    #G_E is the Eshelby propagator. Here, we use it through its FT.
-    G_E_t = -4*a*b / (a+b)**2
-    
-    if (method == 'marko'):
-        G_t = G_E_t
-        G_t[0,0] = 0
-    
-    if (method == 'rossi'):
-        G_t = G_E_t
-        G_t[0,0] = 0 #temporary value to avoid NaN
+    G_t = -4*a*b / q
+    G_t[:, 0] = 0
+    G_t[0, :] = 0
+
+    if imposed == "strain":
         g = -1/(L**2 - 1) * (np.sum(G_t) - G_t[0,0])
-        G_t /= g
-        G_t[0,0] = -1
+    elif imposed == "stress":
+        g = -1/(L**2) * (np.sum(G_t) - G_t[0,0])
+    else:
+        raise ValueError("Unknown imposed quantity: " + str(imposed))
 
-    G = fft.ifft2(G_t)
-    return G.real
+    G_t /= g
+    G_t[0,0] = -1
+
+    G = fft.ifft2(G_t).real
+
+    dx = np.arange(L)
+    dx = np.where(dx > L/2, dx - L, dx)
+
+    # fine-tuning: getting rid of rounding errors
+    if imposed == "strain":
+        for i in range(10):
+            G -= (np.sum(G) - G[0, 0]) / G.size
+            G[0, 0] = -1
+    else:
+        for i in range(10):
+            G -= (np.sum(G) - G[0, 0] - 1) / G.size
+            G[0, 0] = -1
+
+    return G, dx, dx
